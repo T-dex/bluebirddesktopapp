b6c405a3c78ca7595a912f2dc8e17284aedca4ca
T-Dex <dex.m.mills@gmail.com>
2018-06-25 16:01:59 -0600

    Added react devtools to dev

--- bluebirddesktopapp/src/mainelectron.js
+++ bluebirddesktopapp/src/mainelectron.js
@@ -2,19 +2,26 @@ const electron =require('electron');
 const path=require('path');
 const app=electron.app;
 const BrowserWindow= electron.BrowserWindow;
+const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
+
+
 
 
 let mainWindow
 app.on("ready",_=>{
 mainWindow= new BrowserWindow({
-    height:500,
-    width:500
+    height:1500,
+    width:1500
 })
+installExtension(REACT_DEVELOPER_TOOLS)
+    .then((name) => console.log(`Added Extension:  ${REACT_DEVELOPER_TOOLS}`))
+    .catch((err) => console.log('An error occurred: ', err));
 mainWindow.loadURL('http://localhost:3000');
 mainWindow.webContents.openDevTools();
-mainWindow.on('close',_=>
+mainWindow.on('close',_=>{
+    console.log("closed")
     mainWindow=null
-)
+})
 })
 app.on("window-all-closed",_=>{
     if(process.platform!=='darwin'){
