ac90d3d11c0766c4c8b03af609669e8f0ef764d1
T-Dex <dex.m.mills@gmail.com>
2018-09-24 18:30:56 -0600

    Checkout branch picloop

--- bluebirddesktopapp/src/App.js
+++ bluebirddesktopapp/src/App.js
@@ -49,14 +49,12 @@ class App extends Component {
        if(this.state.production[key].access==="admin"){
         mainRef.on('value', snap=>{
           const app=snap.val()
-          console.log(app)
           const users={...app.users};
           const days={...app.days};
           const images={...app.images}
           this.setState({production:{users:users,days:days, images:images}})
         })
-        this.setState({user:email, uid:snapshot.user.uid})   
-          
+        this.setState({user:email, uid:snapshot.user.uid})       
        return this.state.production[key]
         }
       }}
@@ -164,78 +162,103 @@ class App extends Component {
   pictureUpload=(picData)=>{
     const userId=picData.userId
     const date= picData.day
-    const fd= new FormData();
-    fd.append('image', this.state.selectedPics, this.state.selectedPics.name)
-    
-    
     let picName;
     let file;
     let refURL;
+    let picId;
+    const fd= new FormData();
+    const test1=Object.keys(this.state.selectedPics).map(key=>{
+      
+      
+      fd.append('image', this.state.selectedPics[key], this.state.selectedPics[key].name)  
+      const metadata={
+        contentType:'image/jpeg'
+      }
+      picName=this.state.selectedPics[key].name
+      file= this.state.selectedPics[key]
+      
+      let newRef=storageRef.child(picName).put(file, metadata).then(snapshot=>snapshot.ref.getDownloadURL().then(downloadURL=>{
+        refURL=downloadURL
+        let picId;
+       mainRef.child('images').on('value',snap=>picId=snap.val()
+       ) 
+       const newUserId=Object.keys(picId).filter(key=>key==userId
+       )
+       console.log(newUserId.length, userId);
+       
+      //  console.log(refURL, [key]);
+      //  console.log(picId);
+       
+        //Getting somewhere. If length = 0 user does not exsit and need to build new "state" and insert into images
+
+        
+       // Need to take users and photos and loop over them. If they match then push to a previous state, if not build a new state with new user.
+     
+   
+      
+      //  const test=Object.keys(picId).map(key=>{
+      //   console.log(refURL, [key],"inside");
+      //   const url=refURL
+      //  if(userId===key){
+      //    // would rather not go through another loop to get here but might need to.
+      //    ///causing many issues with inserting photos with out loopping over object
+      //   const newPicture={
+      //     ...this.state.production.images[key],
+      //     [pictureId]:{  
+      //     [pictureId]:{
+      //         [date]:[date],
+      //       url:url}}
+      //   }
+      //   console.log(newPicture,[key]);
+        
+        
+      //   const updatedImageState={
+      //     ...this.state.production.images[key],
+      //     [key]:{[date]:newPicture}
+      //   }
+       
+        
+      //   this.setState(prevState=>({
+      //     ...prevState.production.images[key],
+      //     images:updatedImageState
+      //   }))
+      //  }else{
+      //    const newUserPics={
+      //       [date]:{
+      //        [pictureId]:{
+      //          date:pictureId,
+      //        url:url}}
+      //    }
+      //    const updatedImageState={
+      //      ...this.state.production.images,
+      //      [userId]:newUserPics
+      //    }
+      //    this.setState(prevState=>({
+      //      production:{
+      //        ...prevState.production,
+      //       images:updatedImageState
+      //      }
+      //    }))
+      //   //  mainRef.child('images/').set(updatedImageState)
+         
+      //  }
+      // })
+       
+
+       
+      }))
+    }) 
+    
     const metadata={
       contentType:'image/jpeg'
     }
-    let picId;
-    Object.keys(this.state.selectedPics).map(key=>{
-      picName=this.state.selectedPics[key].name
-      file= this.state.selectedPics[key]
+ 
     // eslint-disable-next-line
     let newRef= storageRef.child(picName).put(file, metadata).then(snapshot=>snapshot.ref.getDownloadURL().then(downloadURL=>{
       refURL=downloadURL
-      let picId;
-     mainRef.child('images').on('value',snap=>picId=snap.val()
-     )
-     console.log(newRef);
      // Images files are being uploaded in buckets Need to map over them and individually create new objects to pump into state
      // eslint-disable-next-line
-     const test=Object.keys(picId).map(key=>{
-      const url=refURL
-     if(userId===key){
-       console.log(key, userId);
-       // would rather not go through another loop to get here but might need to.
-       ///causing many issues with inserting photos with out loopping over object
-      const newPicture={
-        ...this.state.production.images[key],
-        [pictureId]:{  
-        [pictureId]:{
-            [date]:[date],
-          url:url}}
-      }
-      console.log(newPicture, this.state.production.images);
-      
-      const updatedImageState={
-        ...this.state.production.images[key],
-        [key]:{[date]:newPicture}
-      }
-      console.log(updatedImageState,"User exsits");
-      
-      this.setState(prevState=>({
-        ...prevState.production.images[key],
-        images:updatedImageState
-      }))
-     }else{
-      console.log(key, userId);
-       console.log("New User");
-       
-       const newUserPics={
-          [date]:{
-           [pictureId]:{
-             date:pictureId,
-           url:url}}
-       }
-       const updatedImageState={
-         ...this.state.production.images,
-         [userId]:newUserPics
-       }
-       this.setState(prevState=>({
-         production:{
-           ...prevState.production,
-          images:updatedImageState
-         }
-       }))
-       mainRef.child('images/').set(updatedImageState)
-       
-     }
-    })})
+    })
      
 //Logic needs to get figured out here. Just need to set the images up correctly. Will also need to adjust main page as it is not set up right
 //Listening to Aha at sunset coffee if you need help with the head space
@@ -243,7 +266,7 @@ class App extends Component {
       
 
     )
-  })}
+  }
   render() {
   let mainArea;
   if(this.state.user!==null && this.state.page===2){
